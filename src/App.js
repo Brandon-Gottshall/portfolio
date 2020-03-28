3b42a359fc5d7dec6f67ad858470d5e1f35710a9
Brandon Gottshall <Brandon@gottshall.dev>
2020-03-28 17:28:50 -0400

    Curently fixing styling.

--- src/App.js
+++ src/App.js
@@ -7,39 +7,18 @@ import Nav from './components/Nav'
 import Skills from './components/Skills'
 import Languages from './components/Languages'
 import Form from './components/Form.js'
-//Color Imports
+//SAASy imports
+import './styles/styles.css'
 import { red } from './services/colorPallete'
 //Image Imports
 import resume from './images/Resume.png'
 
 export default function App() {
-    const widthSetter = (window.innerWidth > 620) ? ('80vw') : ('100vw')
-    const resumeWidthSetter = (window.innerWidth > 620) ? ('60vw') : ('100vw')
-    const resumeMarginSetter = (window.innerWidth > 620) ? ('20vw') : ('-9vw')
-    const styles = {
-        appContainer: {
-            display: 'flex',
-            justifyContent: 'space-between',
-            borderTop: `2vh solid ${red}`,
-            position: 'fixed',
-            width: '100vw',
-            top: '0',
-            left: '0'
-        },
-        contentScrollBox: {
-            display: 'block',
-            alignItems: 'center',
-            margin: '0',
-            height: '100vh',
-            width: widthSetter,
-            overflowY: 'scroll',
-            overflowX: 'hidden'
-        }
-    }
+
     return (
-        <div style={styles.appContainer}>
-        {(window.innerWidth > 620) ? (<Nav/>) : (null)}
-            <div style={styles.contentScrollBox}>
+        <div className='appContainer'>
+            <Nav/>
+            <div className='contentScrollBox'>
                 <br/>
                 <Profile/>
                 <Divider text='Skills' />
@@ -49,15 +28,40 @@ export default function App() {
                 <br/>
                 <Divider text='Resume' />
                 <br/>
-            <a style={{display: 'flex', justifyContent: 'flex-end', width: '90vw'}} href='https://www.gottshall.dev/Resume.pdf'>
-                    <img style={{marginRight: resumeMarginSetter, width: resumeWidthSetter}} src={resume}/>
-                </a>
-                <Divider text='Contact Me' />
-                <br/>
-                <Form/>
-                <br/>
+                <a href='https://www.gottshall.dev/Resume.pdf'>
+                        
+                    </a>
+                    <Divider text='Contact Me' />
+                    <br/>
+                    <Form/>
+                    <br/>
 
-            </div>
+                </div>
         </div>
     )
 }
+
+
+// const widthSetter = (window.innerWidth > 620) ? ('80vw') : ('100vw')
+// const resumeWidthSetter = (window.innerWidth > 620) ? ('60vw') : ('100vw')
+// const resumeMarginSetter = (window.innerWidth > 620) ? ('20vw') : ('-9vw')
+// const styles = {
+//     appContainer: {
+//         display: 'flex',
+//         justifyContent: 'space-between',
+//         borderTop: `2vh solid ${red}`,
+//         position: 'fixed',
+//         width: '100vw',
+//         top: '0',
+//         left: '0'
+//     },
+//     contentScrollBox: {
+//         display: 'block',
+//         alignItems: 'center',
+//         margin: '0',
+//         height: '100vh',
+//         width: widthSetter,
+//         overflowY: 'scroll',
+//         overflowX: 'hidden'
+//     }
+// }
