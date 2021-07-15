b54e9ca4d2babfdc470600e7cb59ee408f1b534d
Brandon@Effortless <brandon@effortlessdevelopment.com>
2021-07-15 16:06:36 -0400

    added zoom to resume & completed web and mobile styling on resume

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,27 +1,33 @@
-import { LanguageIcon } from './LanguageIcon'
+import { LanguageIcon, iconList } from './LanguageIcon'
 import { useState } from 'react'
 
-export default function ProjectCard () {
+export default function ProjectCard ({ title, description, languages, link, imageURI }) {
   const iconStyle = 'w-4 h-4 ml-1'
-  const [buttonStyle, setButtonStyle] = useState('flex transition-shadow flex-col items-center h-auto sm:rounded-3xl nm-concave-gray-100-lg')
+  const [buttonStyle, setButtonStyle] = useState('flex transition-shadow flex-col items-center h-96 sm:rounded-3xl nm-concave-gray-100-lg')
   const onClickHelper = () => {
     setButtonStyle('flex transition-shadow flex-col items-center h-auto sm:rounded-3xl nm-inset-gray-100-lg')
   }
+  console.dir({ title, description, languages, link, imageURI })
+
   return (
     <div className='flex flex-col justify-center w-1/4 h-auto pb-6 overflow-y-auto sm:py-12 '>
       <div className='w-4/5 py-3 sm:mx-auto'>
         <div className={buttonStyle} onClick={onClickHelper}>
-          <h2 className='my-2 text-3xl font-thin'>Crime NY</h2>
+          <h2 className='my-2 text-3xl font-thin'>{title || 'Crime NY'}</h2>
           <div className='flex items-center justify-center w-4/5 h-auto overflow-visible rounded-2xl'>
             <img className='w-11/12 h-full p-1 rounded-sm nm-flat-gray-400' src='/portfolioScreenshot.png' alt='' />
           </div>
-          <div className='flex flex-col w-4/5 pt-2 space-y-4'>
-            <p className='overflow-y-hidden text-gray-400 max-h-40'>This is a project description. It has many words. It's a placeholder for the description to come.</p>
-            <div className='flex flex-col items-center justify-center 3xl:justify-between 3xl:flex-row'>
-              <div className='flex flex-wrap items-center justify-center w-1/3 pb-4 3xl:justify-end sm:flex-nowrap'>
-                <LanguageIcon iconName='Flutter' iconStyle={iconStyle} />
-                <LanguageIcon iconName='JS' iconStyle={iconStyle} />
-              </div>
+          <div className='flex flex-col justify-between w-4/5 h-full pt-2'>
+            <p className='mt-1 overflow-y-auto text-gray-400 max-h-32'>{description || 'This is a project description. It has many words. It\'s a placeholder for the description to come.'}</p>
+            <div className='flex flex-wrap self-end justify-center w-full pb-2 mb-1'>
+              {languages
+                ? languages.map((language) => {
+                  console.log(`Project: ${title}\nIconName:${language}\nIcon: ${iconList[language]}`)
+                  return (<LanguageIcon key={`key_projectcard_${language}_icon`} iconName={language} iconStyle={iconStyle} />
+                  )
+                })
+                : ([<LanguageIcon key='key_projectcard_ex1_icon' iconName='Flutter' iconStyle={iconStyle} />,
+                  <LanguageIcon key='key_projectcard_ex2_icon' iconName='JS' iconStyle={iconStyle} />])}
             </div>
           </div>
         </div>
--- components/Projects.js
+++ components/Projects.js
@@ -1,9 +1,10 @@
-import DeviceIcon from './DeviceIcon'
+import { useState, useEffect } from 'react'
 import BouncingLink from './BouncingLink'
 import BouncingArrow from './BouncingArrow'
 import ProjectCard from './ProjectCard'
-import { iconList, LanguageIcon } from './LanguageIcon'
+import { LanguageIcon } from './LanguageIcon'
 import Tooltip from './Tooltip'
+import projectsData from '../data/projects.json'
 
 export default function Projects ({ pageNumber, linkSafeguard, setPageNumber, setLinkSafeguard, lockScroll, unlockScroll }) {
   const openPage1 = async () => {
@@ -23,6 +24,24 @@ export default function Projects ({ pageNumber, linkSafeguard, setPageNumber, se
     }, 800)
   }
 
+  const [filters, setFilters] = useState([])
+
+  useEffect(() => {
+    console.log(`filters: ${JSON.stringify(filters, null, 1)}`)
+  }, [filters])
+
+  const languagesUsed = new Set(projectsData.map(({ languages }) => languages.map(language => language)).flat())
+  const languagesUsedArray = Array.from(languagesUsed)
+  console.log('projectsData:')
+  console.dir(projectsData)
+  console.log(`languagesUsed: ${JSON.stringify(languagesUsed, null, 1)}`)
+  console.log(`languagesUsedArray: ${JSON.stringify(languagesUsedArray, null, 1)}`)
+  const addFilter = (language) => {
+    const filterSet = new Set([...filters, language])
+    console.log(`filterSet: ${JSON.stringify(filterSet, null, 1)}`)
+    console.log(`filterSetArray: ${JSON.stringify(Array.from(filterSet), null, 1)}`)
+    setFilters(Array.from(filterSet))
+  }
   return (
     <main className='flex flex-col items-center justify-start w-full h-full px-20 text-center'>
       <div className='flex flex-col items-center justify-center w-full mt-4'>
@@ -36,9 +55,13 @@ export default function Projects ({ pageNumber, linkSafeguard, setPageNumber, se
           >Projects
           </h3>
           <div className={`flex w-auto transform-gpu items-center justify-center space-x-10 py-auto transform ${(linkSafeguard && pageNumber === 1) ? 'translate-x-0 animate-fade-in-from-right' : 'translate-x-off-screen-right duration-500'} w-auto whitespace-nowrap`}>
-            {iconList.map((language) => (
+            {languagesUsedArray.map((language) => (
               <Tooltip key={`key_langTT_${language}`} tooltipText={language}>
-                <LanguageIcon key={`key_langIcon_${language}`} iconName={language} iconStyle='w-10 h-10 transform-gpu hover:animate-bounce' />
+                <div onClick={() => addFilter(language)}>
+                  <LanguageIcon
+                    key={`key_langIcon_${language}`} iconName={language} iconStyle='w-10 h-10 transform-gpu hover:animate-bounce' setFilters={setFilters}
+                  />
+                </div>
               </Tooltip>
             ))}
           </div>
@@ -47,12 +70,7 @@ export default function Projects ({ pageNumber, linkSafeguard, setPageNumber, se
       <div
         onMouseOver={lockScroll} onMouseOut={unlockScroll} className='flex flex-wrap mt-2 mb-8 overflow-y-auto nm-inset-gray-100 rounded-3xl'
       >
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projectsData.filter(({ languages }) => languages.some(languages => languages.includes(filters))).map(({ title, description, languages, link, imageURI }) => <ProjectCard key={`key_${title}_projectCard`} title={title} description={description} languages={languages} link={link} imageURI={imageURI} />)}
       </div>
       <BouncingLink helper={openPage3} linkText='My Resume' direction='down' />
     </main>
@@ -60,6 +78,8 @@ export default function Projects ({ pageNumber, linkSafeguard, setPageNumber, se
 }
 
 // Device Icon Filters
+// import DeviceIcon from './DeviceIcon'
+
 // <div className={`flex w-1/4 items-center justify-center space-x-1 transform ${(linkSafeguard && pageNumber === 1) ? 'translate-x-0 animate-fade-in-from-left' : 'translate-x-off-screen-left duration-500'}  w-96 whitespace-nowrap`}>
 // <DeviceIcon deviceIconStyle linkSafeguard deviceName='web' altText='I develop for web.' tooltipText='Web Development' />
 // <DeviceIcon deviceIconStyle linkSafeguard deviceName='desktop-mobile' altText='I develop for Desktop, tablets, and mobile.' tooltipText='Standalone Software (Desktop, Mobile, Tablet etc.)' />
