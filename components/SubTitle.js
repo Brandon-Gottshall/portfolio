7b51d607fb2311a55550b4fbe039d46c5c9e5542
Brandon@Effortless <brandon@effortlessdevelopment.com>
2021-07-19 14:58:30 -0400

    simplified

--- components/SubTitle.js
+++ components/SubTitle.js
@@ -1,8 +1,8 @@
 
-export default function SubTitle({ title }) {
+export default function SubTitle ({ title }) {
   return (
-    <h3 className="text-3xl font-thin w-96">
+    <h3 className='text-3xl font-thin w-96'>
       {title}
     </h3>
-  );
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,9 @@
 import Head from 'next/head'
-import { useState, useEffect } from 'react'
 import { SocialIcon } from 'react-social-icons'
-import ReactPageScroller from 'react-page-scroller'
-import LandingPage from '../components/LandingPage'
-import Projects from '../components/Projects'
-import Resume from '../components/Resume'
+import SubTitle from '../components/SubTitle'
+import TextLoop from 'react-text-loop'
 
 const Home = () => {
-  const [pageNumber, setPageNumber] = useState(null)
-  const [linkSafeguard, setLinkSafeguard] = useState(true)
-  const [scrollSafeguardUp, setScrollSafeguardUp] = useState(false)
-  const [scrollSafeguardDown, setScrollSafeguardDown] = useState(false)
-
-  const lockScroll = () => {
-    setScrollSafeguardDown(true)
-    setScrollSafeguardUp(true)
-  }
-  const unlockScroll = () => {
-    setScrollSafeguardDown(false)
-    setScrollSafeguardUp(false)
-  }
-
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <Head>
@@ -28,17 +11,24 @@ const Home = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <ReactPageScroller customPageNumber={pageNumber} pageOnChange={setPageNumber} className='flex-grow' blockScrollUp={scrollSafeguardUp} blockScrollDown={scrollSafeguardDown} renderAllPagesOnFirstRender>
-        <LandingPage setPageNumber={setPageNumber} setLinkSafeguard={setLinkSafeguard} />
-        <Projects pageNumber={pageNumber} linkSafeguard={linkSafeguard} setPageNumber={setPageNumber} setLinkSafeguard={setLinkSafeguard} unlockScroll={unlockScroll} lockScroll={lockScroll} />
-        <Resume pageNumber={pageNumber} linkSafeguard={linkSafeguard} setPageNumber={setPageNumber} setLinkSafeguard={setLinkSafeguard} lockScroll={lockScroll} />
-      </ReactPageScroller>
+      <main className='flex flex-col items-center justify-center w-full h-full px-20 text-center'>
+        <div className='flex flex-col items-center justify-center flex-grow w-64 pt-3 whitespace-nowrap'>
+          <h1 className='text-4xl font-bold text-red-500 sm:text-6xl'>Brandon Gottshall</h1>
+          <TextLoop className='transform-gpu' interval={1000}>
+            <SubTitle title='Software Engineer' />
+            <SubTitle title='Former Marine' />
+            <SubTitle title='Entrepreneur' />
+            <SubTitle title='Automation Enthusiast' />
+            <SubTitle title='Second Brain Evangelist' />
+          </TextLoop>
+        </div>
+      </main>
 
       <footer className='flex items-center justify-between w-full h-16 pr-24 border-t nm-convex-white-lg'>
         <div className='flex items-center justify-between h-8 pl-2 pr-16 w-52'>
-          <SocialIcon style={{ width: '4rem' }} url='https://www.linkedin.com/in/brandon-gottshall/' bgColor='#ffffff' fgColor='#000000' />
-          <SocialIcon style={{ width: '4rem' }} url='https://github.com/Brandon-Gottshall' bgColor='#ffffff' fgColor='#000000' />
-          <SocialIcon style={{ width: '4rem' }} url='mailto:blgottshall@gmail.com' bgColor='#ffffff' fgColor='#000000' />
+          <SocialIcon style={{ width: '4rem' }} url='https://www.linkedin.com/in/brandon-gottshall/' bgColor='#1C00ff00' fgColor='#000000' />
+          <SocialIcon style={{ width: '4rem' }} url='https://github.com/Brandon-Gottshall' bgColor='#1C00ff00' fgColor='#000000' />
+          <SocialIcon style={{ width: '4rem' }} url='mailto:blgottshall@gmail.com' bgColor='#1C00ff00' fgColor='#000000' />
         </div>
         <a
           className='flex w-32 whitespace-nowrap'
