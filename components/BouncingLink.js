5a22c958fe958502682d4230eda5afa4d57356a2
Brandon@Effortless <brandon@effortlessdevelopment.com>
2021-07-19 14:46:28 -0400

    mobile styling

--- components/BouncingLink.js
+++ components/BouncingLink.js
@@ -6,7 +6,7 @@ export default function BouncingLink ({ helper, linkText, direction }) {
   }
   const svgClassName = `w-6 h-6 mt-2 transition transform -translate-y-2 fill-current duration-1500 transform-gpu animate-pulse transform-gpu text-amber-900 group-hover:text-red-500 animate-none ${directionToggle[direction]}`
   return (
-    <div className='flex justify-center mt-auto mb-10 transition transform -translate-y-2 duration-1500 transform-gpu group animate-bounce justify-self-end hover:animate-none' onClick={helper}>
+    <div className='flex justify-center pt-8 pb-3 sm:pb-0 sm:transition sm:transform sm:-translate-y-2 sm:h-auto sm:pt-0 sm:mt-auto sm:mb-10 duration-1500 transform-gpu group animate-bounce justify-self-end hover:animate-none' onClick={helper}>
       <svg className={svgClassName} fill='none' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' viewBox='0 0 24 24' stroke='currentColor'>
         <path d='M19 14l-7 7m0 0l-7-7m7 7V3' />
       </svg>
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,37 +1,80 @@
 import { LanguageIcon, iconList } from './LanguageIcon'
 import { useState } from 'react'
+import { ScrollIntoViewIfNeeded } from 'react-scroll-into-view-if-needed'
 
-export default function ProjectCard ({ title, description, languages, link, imageURI }) {
+export default function ProjectCard ({ title, description, languages, link, imageURI, mobile }) {
   const iconStyle = 'w-4 h-4 ml-1'
-  const [buttonStyle, setButtonStyle] = useState('flex transition-shadow flex-col items-center h-96 sm:rounded-3xl nm-concave-gray-100-lg')
-  const onClickHelper = () => {
-    setButtonStyle('flex transition-shadow flex-col items-center h-auto sm:rounded-3xl nm-inset-gray-100-lg')
+  const iconStyleMobile = 'w-10 h-10 ml-1 mb-2'
+  const [buttonStyle, setButtonStyle] = useState('nm-convex-gray-100-sm sm:nm-convex-gray-100-lg')
+  const [flipped, setFlipped] = useState(false)
+  const toggleButtonStyle = () => {
+    setButtonStyle(' nm-inset-gray-100-sm sm:nm-inset-gray-100-lg')
+    setTimeout(() => {
+      setButtonStyle('sm:rounded-3xl nm-convex-gray-100-sm m:nm-convex-gray-100-lg')
+    }, 100)
+  }
+  const cardFlipper = () => {
+    setFlipped(!flipped)
   }
   console.dir({ title, description, languages, link, imageURI })
 
-  return (
-    <div className='flex flex-col justify-center w-1/4 h-auto pb-6 overflow-y-auto sm:py-12 '>
-      <div className='w-4/5 py-3 sm:mx-auto'>
-        <div className={buttonStyle} onClick={onClickHelper}>
-          <h2 className='my-2 text-3xl font-thin'>{title || 'Crime NY'}</h2>
-          <div className='flex items-center justify-center w-4/5 h-auto overflow-visible rounded-2xl'>
-            <img className='w-11/12 h-full p-1 rounded-sm nm-flat-gray-400' src='/portfolioScreenshot.png' alt='' />
-          </div>
-          <div className='flex flex-col justify-between w-4/5 h-full pt-2'>
-            <p className='mt-1 overflow-y-auto text-gray-400 max-h-32'>{description || 'This is a project description. It has many words. It\'s a placeholder for the description to come.'}</p>
-            <div className='flex flex-wrap self-end justify-center w-full pb-2 mb-1'>
-              {languages
-                ? languages.map((language) => {
-                  console.log(`Project: ${title}\nIconName:${language}\nIcon: ${iconList[language]}`)
-                  return (<LanguageIcon key={`key_projectcard_${language}_icon`} iconName={language} iconStyle={iconStyle} />
-                  )
-                })
-                : ([<LanguageIcon key='key_projectcard_ex1_icon' iconName='Flutter' iconStyle={iconStyle} />,
-                  <LanguageIcon key='key_projectcard_ex2_icon' iconName='JS' iconStyle={iconStyle} />])}
+  return mobile
+    ? (
+      <div className='flex flex-col items-end justify-center flex-grow w-full min-h-full sm:min-h-0 sm:overflow-y-auto sm:pt-0 sm:mb-0 sm:mt-1 sm:h-96 sm:w-1/4 sm:py-12 snap-start'>
+        <div className={`w-10/12 mb-8 mr-6 h-5/6 sm:py-3 sm:mx-auto smooth-flip ${flipped ? 'flipped-card' : ''}`} onClick={cardFlipper}>
+          <div className={flipped ? 'flipped-card-inner' : 'flip-card-inner-mobile'}>
+            <div className={`flex flex-col items-center h-full transition-shadow nm-convex-gray-400-sm rounded-3xl ${flipped ? 'flipped-card-front' : 'flip-card-front'} sm:py-4`}>
+              <h2 className='my-2 text-3xl font-thin'>{title || 'Crime NY'}</h2>
+              <div className='flex items-center justify-center w-full h-auto overflow-visible rounded-2xl'>
+                <img className='w-11/12 h-full p-1 rounded-sm' src='/portfolioScreenshot.png' alt='' />
+              </div>
+            </div>
+            <div className={`flex flex-col items-center transition-shadow nm-convex-gray-400-sm rounded-3xl ${flipped ? 'flipped-card-back' : 'flip-card-back'} sm:py-4 rounded-3xl`}>
+              <div className='flex flex-col justify-between w-11/12 h-full pt-2'>
+                <p className='mt-4 overflow-y-auto text-left text-white max-h-32'>{description || 'This is a project description. It has many words. It\'s a placeholder for the description to come.'}</p>
+                <div className='flex flex-wrap self-end justify-center w-full h-auto pb-2 mb-1'>
+                  {languages
+                    ? languages.map((language) => {
+                      console.log(`Project: ${title}\nIconName:${language}\nIcon: ${iconList[language]}`)
+                      return (<LanguageIcon key={`key_projectcard_${language}_icon`} iconName={language} iconStyle={iconStyleMobile} />
+                      )
+                    })
+                    : ([<LanguageIcon key='key_projectcard_ex1_icon' iconName='Flutter' iconStyle={iconStyle} />,
+                      <LanguageIcon key='key_projectcard_ex2_icon' iconName='JS' iconStyle={iconStyle} />])}
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+      )
+    : (
+      <div className='flex flex-col items-center justify-center flex-grow w-full min-h-full sm:min-h-0 sm:overflow-y-auto sm:pt-0 sm:mb-0 sm:mt-1 sm:h-96 sm:w-1/4 sm:py-12 snap-start'>
+        <div className='w-4/5 mb-8 h-5/6 sm:py-3 sm:mx-auto smooth-flip flip-card'>
+          <div className={'flip-card-inner ' + buttonStyle} onClick={toggleButtonStyle}>
+            <div className='flex flex-col items-center h-full transition-shadow flip-card-front sm:py-4 rounded-3xl'>
+              <h2 className='my-2 text-3xl font-thin'>{title || 'Crime NY'}</h2>
+              <div className='flex items-center justify-center w-4/5 h-auto overflow-visible rounded-2xl'>
+                <img className='w-11/12 h-full p-1 rounded-sm nm-flat-gray-400' src='/portfolioScreenshot.png' alt='' />
+              </div>
+            </div>
+            <div className='flex flex-col items-center transition-shadow flip-card-back sm:py-4 rounded-3xl'>
+              <div className='flex flex-col justify-between w-4/5 h-full pt-2'>
+                <p className='mt-1 overflow-y-auto text-gray-400 max-h-32'>{description || 'This is a project description. It has many words. It\'s a placeholder for the description to come.'}</p>
+                <div className='flex flex-wrap self-end justify-center w-full h-auto pb-2 mb-1'>
+                  {languages
+                    ? languages.map((language) => {
+                      console.log(`Project: ${title}\nIconName:${language}\nIcon: ${iconList[language]}`)
+                      return (<LanguageIcon key={`key_projectcard_${language}_icon`} iconName={language} iconStyle={iconStyle} />
+                      )
+                    })
+                    : ([<LanguageIcon key='key_projectcard_ex1_icon' iconName='Flutter' iconStyle={iconStyle} />,
+                      <LanguageIcon key='key_projectcard_ex2_icon' iconName='JS' iconStyle={iconStyle} />])}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      )
 }
