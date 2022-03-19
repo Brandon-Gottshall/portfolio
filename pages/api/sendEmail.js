598d5c2b1fa2ee2b0ee92ea055732b6355d153cb
Brandon@Effortless <brandon@effortlessdevelopment.com>
2022-03-19 17:26:55 -0400

    build send email function wip 2

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function sendEmail (req, res) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID)
   const { email, number, inquiry } = req.body
   console.log({ email, number, inquiry })
   const message = {
@@ -11,5 +12,12 @@ export default function sendEmail (req, res) {
     html: `<p>Email: ${email}</p><p>Number: ${number}</p><p>Inquiry: ${inquiry}</p>`
   }
   console.log(message)
+  sgMail.send(message).send(message)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
   res.end(JSON.stringify({ test: 'test', email, number, inquiry }))
 }
