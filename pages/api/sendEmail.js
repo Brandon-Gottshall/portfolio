a06c940f21a0ebfb8b6ba57c98f7aa5736948284
Brandon@Effortless <brandon@effortlessdevelopment.com>
2022-06-17 15:12:12 -0400

    Update sendEmail.js

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,15 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import sgMail from '@sendgrid/mail'
-export default function sendEmail (req, res) {
-  const { email, phoneNumber, inquiry } = JSON.parse(req.body)
+// import sgMail from '@sendgrid/mail'
+export default async function sendEmail (req, res) {
+  const { email, phoneNumber, inquiry } = req.body
   const message = {
     from: 'inquiry@brandongottshall.com',
     to: 'blgottshall@gmail.com',
     subject: 'Portfolio Inquiry',
     html: `<p>Email: ${email}</p><p>Number: ${phoneNumber}</p><p>Inquiry: ${inquiry}</p>`
   }
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  sgMail.send(message).catch((error) => {
+  await sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  await sgMail.send(message).catch((error) => {
     console.error(error)
     console.error(JSON.stringify(error.response.body, null, 2))
   })
